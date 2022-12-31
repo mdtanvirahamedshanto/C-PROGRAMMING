ebf56f6ee3d438b6d9465dc1d568492ada877fa2
Md Tanvir Ahamed Shanto <112241372+mdtanvirahamedshanto@users.noreply.github.com>
2022-12-31 13:16:24 +0600

    Calculate the area of a circle in C++

---
+++ Calculate the area of a circle in C++.cpp
@@ -0,0 +1,20 @@
+// Calculate the area of circle
+#include <iostream>
+using namespace std;
+
+// pi = 3.14;
+
+int main()
+{
+
+    float radius,area;
+    cout<<"What is the Radius: ";
+    cin>>radius;
+
+    area = 3.14 * radius * radius;
+
+    cout<<"Area of the circle: ";
+    cout<<area;
+
+return 0;
+}
