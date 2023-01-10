6b00647e53d646079a87f287c36526195426b77f
Md Tanvir Ahamed Shanto <112241372+mdtanvirahamedshanto@users.noreply.github.com>
2023-01-10 12:17:26 +0600

    Output Variable Size

---
+++ clear.c
@@ -0,0 +1,18 @@
+// Output Variable Size 
+
+#include<stdio.h>
+int main()
+{
+    int i;
+    float k;
+    double z;
+    char j;
+
+    printf("Size Of int = %d\n",sizeof(i));
+    printf("Size Of float = %d\n",sizeof(k));
+    printf("Size Of double = %d\n",sizeof(z));
+    printf("Size Of Char = %d \n",sizeof(j));
+
+return 0;
+
+}
